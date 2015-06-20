fe59d1b5fc985ad6e60a4cf829ea9db2699bf4d8
unknown <er230059@gmail.com>
2015-06-21 04:19:55 +0800

    Added full C# project

---
+++ MQTT-Client/MQTT-Client/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MQTT_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HostTextBox = new System.Windows.Forms.TextBox();
+            this.TopicTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.SendMessageTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(37, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(32, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Topic:";
+            // 
+            // HostTextBox
+            // 
+            this.HostTextBox.Location = new System.Drawing.Point(87, 12);
+            this.HostTextBox.MaxLength = 35;
+            this.HostTextBox.Name = "HostTextBox";
+            this.HostTextBox.Size = new System.Drawing.Size(285, 22);
+            this.HostTextBox.TabIndex = 2;
+            // 
+            // TopicTextBox
+            // 
+            this.TopicTextBox.Location = new System.Drawing.Point(87, 40);
+            this.TopicTextBox.MaxLength = 35;
+            this.TopicTextBox.Name = "TopicTextBox";
+            this.TopicTextBox.Size = new System.Drawing.Size(285, 22);
+            this.TopicTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(361, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(361, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 447);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(361, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(11, 154);
+            this.MessageTextBox.Multiline = true;
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.ReadOnly = true;
+            this.MessageTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.MessageTextBox.Size = new System.Drawing.Size(361, 287);
+            this.MessageTextBox.TabIndex = 7;
+            // 
+            // SendMessageTextBox
+            // 
+            this.SendMessageTextBox.Location = new System.Drawing.Point(87, 97);
+            this.SendMessageTextBox.MaxLength = 35;
+            this.SendMessageTextBox.Name = "SendMessageTextBox";
+            this.SendMessageTextBox.Size = new System.Drawing.Size(285, 22);
+            this.SendMessageTextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(8, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Message:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(12, 473);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 501);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SendMessageTextBox);
+            this.Controls.Add(this.MessageTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TopicTextBox);
+            this.Controls.Add(this.HostTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MQTT Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox HostTextBox;
+        private System.Windows.Forms.TextBox TopicTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.TextBox SendMessageTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MQTT-Client/MQTT-Client/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTT_Client
+{
+    public partial class Form1 : Form
+    {
+        private delegate void myUICallBack(string myStr, TextBox ctl);
+        static MqttClient client;
+        private void myUI(string myStr, TextBox ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUICallBack myUpdate = new myUICallBack(myUI);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.AppendText(myStr + Environment.NewLine);
+            }
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            myUI(System.Text.Encoding.UTF8.GetString(e.Message), MessageTextBox);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = this.button1;
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null && client.IsConnected) client.Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SendMessageTextBox.Text.Length == 0)
+            {
+                label4.Text = "No message to send";
+            }
+            else
+            {
+                label4.Text = "";
+                client.Publish(TopicTextBox.Text, Encoding.UTF8.GetBytes(SendMessageTextBox.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (HostTextBox.Text.Length == 0)
+            {
+                label4.Text = "Invild host";
+            }
+            else if (TopicTextBox.Text.Length == 0)
+            {
+                label4.Text = "You need have topic to subscribe";
+            }
+            else
+            {
+                try
+                {
+                    client = new MqttClient(HostTextBox.Text);
+                    client.Connect(Guid.NewGuid().ToString());
+                    client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(client_MqttMsgPublishReceived);
+                }
+                catch
+                {
+                    label4.Text = "Can't connect to server";
+                }
+                if (client != null && client.IsConnected)
+                {
+                    label4.Text = "";
+                    client.Subscribe(new string[] { TopicTextBox.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                    HostTextBox.Enabled = false;
+                    TopicTextBox.Enabled = false;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageTextBox.Clear();
+        }
+    }
+}
